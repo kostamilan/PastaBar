46aa488e567c991deed2722fe554919556fccd26
Milan Kostadinovic <kostadinovicnis@gmail.com>
2022-01-08 16:50:50 +0100

    Dodavanje potrebnih nizova

--- src/pastaBar.java
+++ src/pastaBar.java
@@ -2,7 +2,11 @@
 public class pastaBar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String [] stalneMusterije = {"0631111111", "063222222", "063333333", "064444444", "065555555", "066666666"};
+		String [] sastojci = {"Makarone", "Spagete", "Bolognese", "Curetina", "Govedja prsuta", "Slanina",
+							"Piletina", "4 sira", "Dimljeni sir", "Parmezan", "Pavlaka", "Pesto sos",
+							"Napolitana", "Povrce mix", "Pecurke", "Kutija"};
+		int [] cena = {50, 60 , 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20};
 	}
 	
 	public static int findIngredient(String[] ingredients, String ingredientName) {
