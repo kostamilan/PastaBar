0d8555f40cba29620490100a986510fa178ed436
Milan Kostadinovic <kostadinovicnis@gmail.com>
2022-01-08 16:43:22 +0100

    Projekat PastaBar

--- src/pastaBar.java
+++ src/pastaBar.java
@@ -3,7 +3,17 @@ public class pastaBar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	}
+	
+	public static int findIngredient(String[] ingredients, String ingredientName) {
+		
+		return 0;
+	}
+	
+	
+	public static boolean isRegularCustomer(String[] phoneNumbers, String phone) {
+		
+		return false;
 	}
 
 }
